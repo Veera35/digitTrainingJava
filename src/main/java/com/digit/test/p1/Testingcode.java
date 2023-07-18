dfd2dbe5218fa95496bccd2d9bc38c0a0730f66f
Veera.Gajula <Veera.Gajula@DIGTBGL-L6987.godigit.corp>
2023-07-18 12:34:27 +0530

    Changes

--- src/main/java/com/digit/test/p1/Testingcode.java
+++ src/main/java/com/digit/test/p1/Testingcode.java
@@ -1,5 +1,11 @@
 package com.digit.test.p1;
 
 public class Testingcode {
+	public static void main(String[] args) {
+		System.out.println("hello veera");
+		System.out.println("hello veera");
+		System.out.println("hello veera");
+		System.out.println("hello veera");
+	}
 
 }
